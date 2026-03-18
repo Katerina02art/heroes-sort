0b14d7d8a64b57ed1f4aef024566c62d9e44b619
Katerina02art <artemovaekaterina317@gmail.com>
2026-03-18 13:23:17 +0400

    Fix: rename test folder to __tests__ and recreate test file

--- src/js/sortHeroes.js
+++ src/js/sortHeroes.js
@@ -1,3 +1,3 @@
-﻿export default function sortHeroes(heroes) {
-  return [...heroes].sort((a, b) => b.health - a.health);
-}
+export default function sortHeroes(heroes) { 
+  return [...heroes].sort((a, b) && b.health - a.health); 
+} 
